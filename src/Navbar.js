dc2bb154ec39498eba090c7cc2d20fd22d1ca998
Gus Beringer <berin013@umn.edu>
2022-03-29 17:18:21 -0500

    navbar

---
+++ src/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Navbar.css'
+import pdf from './files/Resume.pdf'
+
+const Navbar = () => {
+  return (
+    <>
+    <h1 className='site-title'>Gus Beringer</h1>
+    <nav>
+      <ul className='nav-container'>
+        <li className="nav-item">
+          LinkedIn
+        </li>
+        <li className="nav-item">
+          <a href="https://www.github.com/gusberinger">Github</a>
+        </li>
+        <li className="nav-item">
+          <a href={pdf} download="resume.pdf">Resume</a>
+        </li>
+        <li className="nav-item">
+          <a href="mailto:gusberinger">CONTACT</a>
+        </li>
+      </ul>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
