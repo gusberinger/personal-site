aed52120ad580bc4c110d7b1b8b84a464bb15fdc
Gus Beringer <berin013@umn.edu>
2022-03-29 19:52:44 -0500

    changed movie list to ids

--- src/Main.js
+++ src/Main.js
@@ -5,20 +5,9 @@ import waters from './images/waters.jpeg'
 import Movie from './Movie'
 
 const movies = [
-  "The Tragedy of Macbeth",
-  "Uncut Gems",
-  "Eraserhead",
-  "The Killing of a Sacred Deer",
-  "The Lighthouse",
-  "The Handmaiden",
-  "Snowpiercer",
-  "In Bruges",
-  "Silence",
-  "Whiplash",
-  "Moonrise Kingdom",
-  "Dunkirk",
-  "The Raid",
-  "Rabbits"
+  "tt0068646",
+  "tt5715874",
+  "tt8772262"
 ]
 
 const selectMovie = () => {
@@ -38,13 +27,12 @@ const Main = () => {
         I'm a mathematics student at the University of Minnesota Twin Cities, also completing minors in computer science and political science. Right now I'm researching the Wikipedia hyperlink graph. My favorite tools are Python, Haskell, and the Tidyverse.
       </p>
       <p>
-        Today my favorite movie is {selectMovie()}.
+        Today my favorite movie is <Movie id={selectMovie()}></Movie>.
       </p>
       </div>
       <div className="col">
         <img className="img-content" src={waters} alt=""/>
       </div>
-      {/* <Movie/> */}
 
     </div>
   )
