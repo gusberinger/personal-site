f63021362feed46613b4f021405653dd92ec69ee
Gus Beringer <gusberinger@users.noreply.github.com>
2022-04-10 22:30:16 -0500

    Develop (#4)
    * auto format
    
    * added max-width for section
    
    * changed font-face
    
    * fixed font errors
    
    * only render Movie if data not null
    
    * format document
    
    * added section padding
    
    * added footer

--- src/Movie.js
+++ src/Movie.js
@@ -1,26 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
+const Movie = ({ id }) => {
+  const [data, setData] = useState(null);
 
-const Movie = ({id}) => {
-  const [data, setData] = useState({title:"The Godfather", year: "1920"})
-  
   useEffect(() => {
-    getData(id)
-  }, [id])
+    getData(id);
+  }, [id]);
 
   async function getData(id) {
-    let url = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API}&i=${id}`
-    const response = await fetch(url)
-    const data = await response.json()
-    const { Title, Year } = data
-    setData({ title: Title, year: Year })
+    let url = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API}&i=${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const { Title, Year } = data;
+    setData({ title: Title, year: Year });
   }
 
   return (
     <>
-      <a href={"https://www.imdb.com/title/"+id}>{data.title} ({data.year})</a>
+      {data != null && (
+        <a href={"https://www.imdb.com/title/" + id}>
+          {data.title} ({data.year})
+        </a>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Movie
+export default Movie;
