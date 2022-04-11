3e4e88eb5433c196f1a2d2a13f7bc524240355f4
Gus Beringer <berin013@umn.edu>
2022-04-10 21:39:25 -0500

    format document

--- src/Movie.js
+++ src/Movie.js
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
+const Movie = ({ id }) => {
+  const [data, setData] = useState(null);
 
-const Movie = ({id}) => {
-  const [data, setData] = useState(null)
-  
   useEffect(() => {
-    getData(id)
-  }, [id])
+    getData(id);
+  }, [id]);
 
   async function getData(id) {
-    let url = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API}&i=${id}`
-    const response = await fetch(url)
-    const data = await response.json()
-    const { Title, Year } = data
-    setData({ title: Title, year: Year })
+    let url = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API}&i=${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const { Title, Year } = data;
+    setData({ title: Title, year: Year });
   }
 
   return (
     <>
-    {
-      (data != null) &&
-      <a href={"https://www.imdb.com/title/"+id}>{data.title} ({data.year})</a>
-    }
+      {data != null && (
+        <a href={"https://www.imdb.com/title/" + id}>
+          {data.title} ({data.year})
+        </a>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Movie
+export default Movie;
