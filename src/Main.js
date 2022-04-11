83db7e98585bea54e7089e9631793b80c9bcc3b8
Gus Beringer <gusberinger@users.noreply.github.com>
2022-04-10 19:17:39 -0500

    centered columns, fixed mobile navbar (#3)

--- src/Main.js
+++ src/Main.js
@@ -1,37 +1,39 @@
-import React from 'react'
-import seedrandom from 'seedrandom'
-import './Main.css'
-import waters from './images/waters.jpeg'
-import Movie from './Movie'
-import movieList from './MovieList.js'
-
+import React from "react";
+import seedrandom from "seedrandom";
+import "./Main.css";
+import waters from "./images/waters.jpeg";
+import Movie from "./Movie";
+import movieList from "./MovieList.js";
 
 const selectMovie = () => {
-  let now = new Date()
-  let today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
-  let rng = seedrandom(today)
-  let n = Math.floor(rng() * movieList.length)
-  return movieList[n]
-}
-
+  let now = new Date();
+  let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  let rng = seedrandom(today);
+  let n = Math.floor(rng() * movieList.length);
+  return movieList[n];
+};
 
 const Main = () => {
   return (
-    <div className="content">
-      <div className='col'>
-      <p>
-        I'm a mathematics student at the University of Minnesota Twin Cities, also completing minors in computer science and political science. Right now I'm researching the Wikipedia hyperlink graph. My favorite tools are Python, Haskell, and the Tidyverse.
-      </p>
-      <p>
-        Today my favorite movie is <Movie id={selectMovie()}></Movie>.
-      </p>
+    <section>
+      <div className="row">
+        <div className="col">
+          <p>
+            I'm a mathematics student at the University of Minnesota Twin
+            Cities, also completing minors in computer science and political
+            science. Right now I'm researching the Wikipedia hyperlink graph. My
+            favorite tools are Python, Haskell, and the Tidyverse.
+          </p>
+          <p>
+            Today my favorite movie is <Movie id={selectMovie()}></Movie>.
+          </p>
+        </div>
+        <div className="col">
+          <img className="img-content" src={waters} alt="" />
+        </div>
       </div>
-      <div className="col">
-        <img className="img-content" src={waters} alt=""/>
-      </div>
+    </section>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Main
+export default Main;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import './Navbar.css'
-import pdf from './files/Resume.pdf'
+import React from "react";
+import "./Navbar.css";
+import pdf from "./files/Resume.pdf";
 
 const Navbar = () => {
   return (
-    <>
-    <h1 className='site-title'>Gus Beringer</h1>
-    <nav>
-      <ul className='nav-container'>
-        <li className="nav-item">
-          <a href="https://www.linkedin.com/in/gusberinger/">LinkedIn</a> 
-        </li>
-        <li className="nav-item">
-          <a href="https://www.github.com/gusberinger">Github</a>
-        </li>
-        <li className="nav-item">
-          <a href={pdf} download="resume.pdf">Resume</a>
-        </li>
-        <li className="nav-item">
-          <a href="mailto:gusberinger@gmail.com">CONTACT</a>
-        </li>
-      </ul>
-    </nav>
-    </>
-  )
-}
+    <header>
+      <h1 className="site-title">Gus Beringer</h1>
+      <nav>
+        <ul className="nav-container">
+          <li className="nav-item">
+            <a href="https://www.linkedin.com/in/gusberinger/">LinkedIn</a>
+          </li>
+          <li className="nav-item">
+            <a href="https://www.github.com/gusberinger">Github</a>
+          </li>
+          <li className="nav-item">
+            <a href={pdf} download="resume.pdf">
+              Resume
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="mailto:gusberinger@gmail.com">CONTACT</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
