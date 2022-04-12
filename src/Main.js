ec54ff0d73c12acd7751ff05f0da53fad9995672
Gus Beringer <berin013@umn.edu>
2022-04-12 12:15:56 -0500

    wrote project descriptions

--- src/Main.js
+++ src/Main.js
@@ -4,8 +4,8 @@ import "./Main.css";
 import waters from "./images/waters.jpeg";
 import Movie from "./Movie";
 import movieList from "./MovieList.js";
-import Footer from "./Footer"
-import Navbar from "./Navbar"
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 const selectMovie = () => {
   let now = new Date();
@@ -18,55 +18,68 @@ const selectMovie = () => {
 const Main = () => {
   return (
     <>
-    <Navbar></Navbar>
-    <section>
-      <div className="container">
-        <div className="row hero">
-          <div className="col">
-            <p>
-              I'm a mathematics student at the University of Minnesota Twin
-              Cities, also completing minors in computer science and political
-              science. Right now I'm researching the Wikipedia hyperlink graph. My
-              favorite tools are Python, Haskell, and the Tidyverse.
-            </p>
-            <p>
-              Today my favorite movie is <Movie id={selectMovie()}></Movie>.
-            </p>
-          </div>
-          <div className="col">
-            <img className="img-content" src={waters} alt="" />
-          </div>
-        </div>
-      </div>
-    </section>
-    <section>
-      <div className="container">
-        <h1>Projects</h1>
-        <div class="row">
-          <div class="col">
-            <div class="item">
-              <h3>WikiNetworks</h3>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo rem sint excepturi tempore modi ducimus in maiores eligendi facere cupiditate!</p>
+      <Navbar></Navbar>
+      <section>
+        <div className="container">
+          <div className="row hero">
+            <div className="col">
+              <p>
+                I'm a mathematics student at the University of Minnesota Twin
+                Cities, also completing minors in computer science and political
+                science. Right now I'm researching the Wikipedia hyperlink
+                graph. My favorite tools are Python, Haskell, and the Tidyverse.
+              </p>
+              <p>
+                Today my favorite movie is <Movie id={selectMovie()}></Movie>.
+              </p>
             </div>
-            <div class="item">
-              <h3>Smiter</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis repudiandae architecto fuga molestiae, necessitatibus accusamus omnis corrupti dolores voluptatem dolorem!</p>
-            </div>
-          </div>
-          <div class="col">
-            <div class="item">
-              <h3>Letterboxd-Convert</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis consequatur voluptates corrupti commodi dolor iusto. Possimus sit perspiciatis animi consequuntur!</p>
-            </div>
-            <div class="item">
-              <h3>SafeU Map</h3>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt omnis, dignissimos minus nostrum alias possimus iusto necessitatibus id aliquam aliquid?</p>
+            <div className="col">
+              <img className="img-content" src={waters} alt="" />
             </div>
           </div>
         </div>
-      </div>
-    </section>
-    <Footer></Footer>
+      </section>
+      <section>
+        <div className="container">
+          <h1>Projects</h1>
+          <div class="row">
+            <div class="col">
+              <div class="item">
+                <h3>WikiNetworks</h3>
+                <p>
+                  Network analysis of Wikipedia's hyperlink structure. The graph is created by parsing Wikipedia data dumps into a sparse matrix. Using scipy csr matrices, implemented centrality, and other graph measures. <a href="https://github.com/gusberinger/WikiNetworks">Link</a>
+                </p>
+              </div>
+              <div class="item">
+                <h3>Smiter</h3>
+                <p>
+                  React App that uses form data to automatically roll damage for
+                  Palladins in the board game D&D. <a href="https://smiter.gusberinger.com/">Link</a>
+                </p>
+              </div>
+            </div>
+            <div class="col">
+              <div class="item">
+                <h3>Letterboxd-Convert</h3>
+                <p>
+                  Python package that converts list of movies on Letterboxd.com,
+                  to list of IMDb ids. The lists are downloaded asynchronous and
+                  cached in a local database. <a href="https://github.com/letterboxd-convert">Link</a>
+                </p>
+              </div>
+              <div class="item">
+                <h3>SafeU Map</h3>
+                <p>
+                  Map of all SafeU crime notifications. Each notification
+                  geocoded using Google Maps API. Map created with leaflet
+                  package in R. <a href="https://safeu.gusberinger.com">Link</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer></Footer>
     </>
   );
 };
