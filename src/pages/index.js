3c301b82d3baa78eefbce8d8a48fb640168a3c71
Gus Beringer <berin013@umn.edu>
2022-04-18 16:39:24 -0500

    changed to gatsby index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from "react"
 import Main from "../components/Main"
-import reportWebVitals from '../components/reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>,
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// markup
+const IndexPage = () => {
+  return (
+    <Main></Main>
+  )
+}
+
+export default IndexPage
